e6c9fe7f4b9d28d8620ad29d26256eccae024b1c
Cameron Dykes <yellow5c@gmail.com>
2017-03-27 22:43:26 -0500

    Identify the project

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule Issues.Mixfile do
     [app: :issues,
      escript: escript_config(),
      version: "0.1.0",
+     name: "Issues",
+     source_url: "https://github.com/yellow5/elixir-issues",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
